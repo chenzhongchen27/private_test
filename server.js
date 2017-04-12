2f88fc8c6c29d10831bba11d7ae620731ee7b7ef
xuefei <xuefei@beisen.com>
2017-04-12 09:02:04 +0800

    修改为post信息处理

--- server.js
+++ server.js
@@ -10,34 +10,40 @@ app.listen(PORT,'0.0.0.0',function(){
 var locationObjHistory = []
 
 function handler(req, res) {
-	var urlObj = url.parse(req.url,true)
-	var pathname = urlObj.pathname;
+	// var urlObj = url.parse(req.url,true)
+	// var pathname = urlObj.pathname;
 	//   /set?location=111222
-	console.log('url--',req.url,'method--',req.method,'headers--',req.headers)
-	let content='';
-	req.on('data',function(chunk){
-		content += chunk;
-	})
-	req.on('end',function(){
-		locationData=content;
-		console.log('content--',content)
-		res.writeHead(200)
-		res.end('debugger haha')
-	})
-	return;
-	if(pathname==='/set'){
-		var locationObj = {
-			time:((new Date()).toLocaleString()),
-			location:urlObj.query.location
-		}
-		locationObjHistory.unshift(locationObj)
-		if(locationObjHistory.length>10){
-			//只保留十个历史
-			locationObjHistory=locationObjHistory.splice(0,10)
-		}
-		io.emit('change location',locationObj)
-		res.writeHead(200)
-		res.end('success set data')
+	// console.log('url--',req.url,'method--',req.method,'headers--',req.headers)
+	// let content='';
+	// req.on('data',function(chunk){
+	// 	content += chunk;
+	// })
+	// req.on('end',function(){
+	// 	locationData=content;
+	// 	console.log('content--',content)
+	// 	res.writeHead(200)
+	// 	res.end('debugger haha')
+	// })
+	// return;
+	if(req.method==='POST'){
+		let content='';
+		req.on('data',function(chunk){
+			content += chunk;
+		})
+		req.on('end',function(){
+			var locationObj = {
+				time:((new Date()).toLocaleString()),
+				location:content
+			}
+			locationObjHistory.unshift(locationObj)
+			if(locationObjHistory.length>10){
+				//只保留十个历史
+				locationObjHistory=locationObjHistory.splice(0,10)
+			}
+			io.emit('change location',locationObj)
+			res.writeHead(200)
+			res.end('success set data')
+		})
 		return;
 	}
 	fs.createReadStream('./index.html').pipe(res)
